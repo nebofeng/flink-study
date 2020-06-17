682c2ef52d16cf3fcab4e9765aed500d24483366
nebofeng <nebofeng@gmail.com>
2020-06-17 21:59:42 +0800

    [ADD] keyedstate : demo

---
+++ src/main/java/pers/nebo/streaming/state/keyedstate/Constants.java
@@ -0,0 +1,14 @@
+package pers.nebo.streaming.state.keyedstate;
+
+/**
+ * @ auther fnb
+ * @ email nebofeng@gmail.com
+ * @ date  2020/6/17
+ * @ des :
+ */
+public class Constants {
+
+    public static final String ORDER_INFO1_PATH="F:\\Project\\intellijidea\\flink-study\\data\\orderinfo1";
+    public static final String ORDER_INFO2_PATH="F:\\Project\\intellijidea\\flink-study\\data\\orderinfo2";
+
+}
---
+++ src/main/java/pers/nebo/streaming/state/keyedstate/FileSource.java
@@ -0,0 +1,65 @@
+package pers.nebo.streaming.state.keyedstate;
+
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ auther fnb
+ * @ email nebofeng@gmail.com
+ * @ date  2020/6/17
+ * @ des :
+ */
+public class FileSource implements SourceFunction<String> {
+    public  String filePath;
+
+    public FileSource(String filePath){
+        this.filePath=filePath;
+    }
+
+    private InputStream inputStream;
+    private BufferedReader reader;
+
+    private Random random = new Random();
+
+    @Override
+    public void run(SourceContext<String> ctx) throws Exception {
+        reader=new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
+
+        String line=null;
+        while ((line=reader.readLine())!=null){
+
+            TimeUnit.MILLISECONDS.sleep(random.nextInt(500));
+            ctx.collect(line);
+        }
+        if(reader!=null){
+            reader.close();
+        }
+
+        if(inputStream!=null){
+            inputStream.close();
+        }
+
+
+    }
+
+    @Override
+    public void cancel() {
+        try {
+            if(reader!=null){
+                reader.close();
+            }
+            if(inputStream!=null){
+                inputStream.close();
+            }
+        }catch (Exception e){
+
+        }
+
+    }
+}
